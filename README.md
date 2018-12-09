44922363d40427890b3a0776c113a6c55c7202f3
haraduka <kkinorinnn444@gmail.com>
2018-12-09 19:25:07 +0900

    add cubic_spline.py

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ My practice of robotics easy implementation.
     -  [Models of Trajectory Formation and Temporal Interaction of Reach and Grasp](https://www.tandfonline.com/doi/abs/10.1080/00222895.1993.9942048)
   - `lagrange_polynomial.py`
     - [Lagrange polynomial](https://en.wikipedia.org/wiki/Lagrange_polynomial)
+  - `cubic_spline.py`
+    - [cubic spline (ja)](http://www.yamamo10.jp/yamamoto/lecture/2006/5E/interpolation/interpolation_html/node3.html)
---
+++ interpolation/cubic_spline.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class CubicSplineInterpolation:
+    def __init__(self):
+        print("# initializing...")
+
+        # for drawing
+        self.x_seq = []
+        self.y_seq = []
+
+        print("# initialized.")
+
+    def u_j(self, points, j, x):
+        ans = 1.0
+        for i, p in enumerate(points):
+            if i != j:
+                ans *= (x-p[0])/(points[j][0]-p[0])
+        return ans
+
+    def run(self, points, x_interval):
+        N = len(points)-1
+        px = np.array(points)[:, 0]
+        py = np.array(points)[:, 1]
+        h = []
+        for x0, x1 in zip(px[:-1], px[1:]):
+            h.append(x1-x0)
+        h = np.array(h)
+        assert(len(h) == N)
+        v = []  # 1-indexed
+        for y0, y1, y2, h0, h1 in zip(py[0:-2], py[1:-1], py[2:], h[:-1], h[1:]):
+            v.append(6.*((y2-y1)/h1-(y1-y0)/h0))
+        v = np.array(v)
+        assert(len(v) == N-1)
+
+        mat_for_u = np.zeros((N-1, N-1))
+        for i in range(N-1):
+            for j in range(N-1):
+                if i == j:
+                    mat_for_u[i][j] = 2.*(h[i]+h[i+1])
+                elif i == j+1:
+                    mat_for_u[i][j] = h[i]
+                elif j == i+1:
+                    mat_for_u[i][j] = h[j]
+        v_for_u = v.reshape(len(v), 1)
+
+        u = np.linalg.inv(mat_for_u).dot(v_for_u)
+        u = u.flatten()
+        u = np.concatenate([[0.], u, [0.]])
+
+        b = u[:-1]/2.0
+
+        a = (u[1:]-2.0*b)/(6.0*h)
+
+        d = py[:-1]
+
+        c = 1.0/h*(py[1:]-a*h**3-b*h**2-d)
+        print(a)
+        print(b)
+        print(c)
+        print(d)
+
+        for i, (x0, y0, x1, y1) in enumerate(zip(px[:-1], py[:-1], px[1:], py[1:])):
+            print(x0, x1)
+            for x in np.arange(x0, x1, x_interval):
+                y = a[i]*(x-x0)**3 + b[i]*(x-x0)**2 + c[i]*(x-x0) + d[i]
+                self.x_seq.append(x)
+                self.y_seq.append(y)
+
+    def draw(self):
+        self.fig = plt.figure()
+        self.xy_plot = self.fig.add_subplot(1, 1, 1)
+        self.xy_plot.set_xlabel("x")
+        self.xy_plot.set_ylabel("y")
+        self.xy_plot.plot(self.x_seq, self.y_seq, color='b')
+        plt.show()
+
+
+def test1():
+    cubicSplineInterpolation = CubicSplineInterpolation()
+    cubicSplineInterpolation.run([(0., 0.), (0.5, 1.), (0.8, 0.2), (1., 0.)], 0.01)
+    cubicSplineInterpolation.draw()
+
+
+def test2():
+    cubicSplineInterpolation = CubicSplineInterpolation()
+    cubicSplineInterpolation.run([(0., 0.), (0.1, 0.1), (0.2, 0.1), (0.3, 0.2),
+                                  (0.5, 1.), (0.7, 0.2), (0.8, 0.1), (0.9, 0.1), (1.0, 0.0)], 0.01)
+    cubicSplineInterpolation.draw()
+
+
+def main():
+    test1()
+    test2()
+
+
+if __name__ == '__main__':
+    main()
