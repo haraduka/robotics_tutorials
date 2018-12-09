cdd97261bd99c19f8e394735c87d1b3ef745b7c2
haraduka <kkinorinnn444@gmail.com>
2018-12-09 19:28:42 +0900

    add knot draw to interpolation methods

--- interpolation/cubic_spline.py
+++ interpolation/cubic_spline.py
@@ -10,6 +10,9 @@ class CubicSplineInterpolation:
         self.x_seq = []
         self.y_seq = []
 
+        self.x_cmd_seq = []
+        self.y_cmd_seq = []
+
         print("# initialized.")
 
     def u_j(self, points, j, x):
@@ -23,6 +26,9 @@ class CubicSplineInterpolation:
         N = len(points)-1
         px = np.array(points)[:, 0]
         py = np.array(points)[:, 1]
+        self.x_cmd_seq = px[:]
+        self.y_cmd_seq = py[:]
+
         h = []
         for x0, x1 in zip(px[:-1], px[1:]):
             h.append(x1-x0)
@@ -74,6 +80,7 @@ class CubicSplineInterpolation:
         self.xy_plot.set_xlabel("x")
         self.xy_plot.set_ylabel("y")
         self.xy_plot.plot(self.x_seq, self.y_seq, color='b')
+        self.xy_plot.plot(self.x_cmd_seq, self.y_cmd_seq, color='r')
         plt.show()
 
 
--- interpolation/lagrange_polynomial.py
+++ interpolation/lagrange_polynomial.py
@@ -9,6 +9,8 @@ class LagrangePolynomialInterpolation:
         # for drawing
         self.x_seq = []
         self.y_seq = []
+        self.x_cmd_seq = []
+        self.y_cmd_seq = []
 
         print("# initialized.")
 
@@ -20,6 +22,11 @@ class LagrangePolynomialInterpolation:
         return ans
 
     def run(self, points, x_interval):
+        px = np.array(points)[:, 0]
+        py = np.array(points)[:, 1]
+        self.x_cmd_seq = px[:]
+        self.y_cmd_seq = py[:]
+
         for x in np.arange(points[0][0], points[-1][0], x_interval):
             y = 0.
             for j in range(len(points)):
@@ -33,6 +40,7 @@ class LagrangePolynomialInterpolation:
         self.xy_plot.set_xlabel("x")
         self.xy_plot.set_ylabel("y")
         self.xy_plot.plot(self.x_seq, self.y_seq, color='b')
+        self.xy_plot.plot(self.x_cmd_seq, self.y_cmd_seq, color='r')
         plt.show()
 
 
